d8859cdb422143cf9224bd30c1dde3658bf69cd1
marvinvco <84033742+marvinvco@users.noreply.github.com>
2021-06-16 20:13:00 -0300

    plataforma_educacional

--- js/cursos.js
+++ js/cursos.js
@@ -16,5 +16,33 @@ fetch('https://60b9452d80400f00177b655e.mockapi.io/langs/Cursos')
         let texto_curso = document.createElement("p");
         texto_curso.innerText = curso.texto;
         el_cursos.appendChild(texto_curso);
+
+        let botao_curso = document.createElement("button");
+        botao_curso.innerText = "Favoritar";
+        botao_curso.value = curso.id;
+        el_cursos.appendChild(botao_curso);
+
+        let fav = localStorage.getItem("curso_fav");
+        if(fav){
+            if(fav.includes(curso.id)){
+                botao_curso.disabled = true;
+            }
+        }
+        
+        botao_curso.addEventListener("click", function(e){
+            let curso_fav = localStorage.getItem("curso_fav");
+            
+            if(curso_fav){
+                curso_fav = JSON.parse(curso_fav);
+
+                if(!curso_fav.includes(e.target.value)){
+                    curso_fav.push(e.target.value);
+                    localStorage.setItem("curso_fav", JSON.stringify(curso_fav));
+                }
+            }else{
+                localStorage.setItem("curso_fav", JSON.stringify([e.target.value]));
+            }
+            e.target.disabled = true;
+        });
     });
 });
